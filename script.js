ae713c9257e102614e201e99c1f6a2d0b5857073
shlok2205 <122144636+shlok2205@users.noreply.github.com>
2025-02-06 13:46:09 +0530

    yhbgt

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let songIndex = 0;
 let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
-let gif = document.getElementById('gif');
+let gif = document.getElementById('gif');     
 let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
